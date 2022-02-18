74045bf64e143d12f951f87071dd1c1668902822
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2022-02-18 15:44:55 +0200

    feat: Actually use the error reporting.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,9 +7,9 @@ Template.postSubmit.events({
     };
     Meteor.call("postInsert", post, function (error, result) {
       // display the error to the user and abort
-      if (error) return alert(error.reason);
+      if (error) return throwError(error.reason);
       // show this result but route anyway
-      if (result.postExists) alert("This link has already been posted");
+      if (result.postExists) throwError("This link has already been posted");
       Router.go("postPage", { _id: result._id });
     });
   },
