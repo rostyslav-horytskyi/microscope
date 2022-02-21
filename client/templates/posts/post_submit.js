170123b4173a8d7b1fdc6ec9593fdbd35c026860
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2022-02-21 09:27:21 +0200

    feat: Validate post contents on submission.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,10 +1,13 @@
 Template.postSubmit.events({
   "submit form": function (e) {
     e.preventDefault();
-    const post = {
+    var post = {
       url: $(e.target).find("[name=url]").val(),
       title: $(e.target).find("[name=title]").val(),
     };
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set("postSubmitErrors", errors);
     Meteor.call("postInsert", post, function (error, result) {
       // display the error to the user and abort
       if (error) return throwError(error.reason);
