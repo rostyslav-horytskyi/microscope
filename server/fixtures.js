1d2421277b6ee76cafe2def671d72f68d3c0621c
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2022-03-10 12:39:39 +0200

    feat: Added basic upvoting algorithm.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,22 +1,24 @@
 // Fixture data
 if (Posts.find().count() === 0) {
-  var now = new Date().getTime();
+  const now = new Date().getTime();
   // create two users
-  var tomId = Meteor.users.insert({
+  const tomId = Meteor.users.insert({
     profile: { name: "Tom Coleman" },
   });
-  var tom = Meteor.users.findOne(tomId);
-  var sachaId = Meteor.users.insert({
+  const tom = Meteor.users.findOne(tomId);
+  const sachaId = Meteor.users.insert({
     profile: { name: "Sacha Greif" },
   });
-  var sacha = Meteor.users.findOne(sachaId);
-  var telescopeId = Posts.insert({
+  const sacha = Meteor.users.findOne(sachaId);
+  const telescopeId = Posts.insert({
     title: "Introducing Telescope",
     userId: sacha._id,
     author: sacha.profile.name,
     url: "http://sachagreif.com/introducing-telescope/",
     submitted: new Date(now - 7 * 3600 * 1000),
     commentsCount: 2,
+    upvoters: [],
+    votes: 0,
   });
   Comments.insert({
     postId: telescopeId,
@@ -39,6 +41,8 @@ if (Posts.find().count() === 0) {
     url: "http://meteor.com",
     submitted: new Date(now - 10 * 3600 * 1000),
     commentsCount: 0,
+    upvoters: [],
+    votes: 0,
   });
   Posts.insert({
     title: "The Meteor Book",
@@ -47,16 +51,19 @@ if (Posts.find().count() === 0) {
     url: "http://themeteorbook.com",
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0,
+    upvoters: [],
+    votes: 0,
   });
-
-  for (var i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     Posts.insert({
       title: "Test post #" + i,
       author: sacha.profile.name,
       userId: sacha._id,
       url: "http://google.com/?q=test-" + i,
-      submitted: new Date(now - i * 3600 * 1000),
+      submitted: new Date(now - i * 3600 * 1000 + 1),
       commentsCount: 0,
+      upvoters: [],
+      votes: 0,
     });
   }
 }
