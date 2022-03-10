8346319026e5066be185dd62c5474fb60186e7b3
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2022-03-10 11:43:18 +0200

    feat: Augmented the postsList route to take a limit.

--- server/publications.js
+++ server/publications.js
@@ -1,10 +1,14 @@
-Meteor.publish("posts", function () {
-  return Posts.find();
+Meteor.publish("posts", function (options) {
+  check(options, {
+    sort: Object,
+    limit: Number,
+  });
+  return Posts.find({}, options);
 });
 Meteor.publish("comments", function (postId) {
   check(postId, String);
   return Comments.find({ postId: postId });
 });
 Meteor.publish("notifications", function () {
-  return Notifications.find({ userId: this.userId, read: false });
+  return Notifications.find({ userId: this.userId });
 });
